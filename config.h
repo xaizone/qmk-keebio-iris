06cb264c396b6297f5f87db90bd65d9b03062341
xaizone <git@xaizone.eu>
2023-12-16 17:03:37 +0100

    feat: init commit

---
+++ config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/* set split hand pin */
+#define SPLIT_HAND_PIN D5
+
+/* disable debugging features */
+#define NO_DEBUG
+
+/* disable printing features */
+#define NO_PRINT
+
+/* how long before a tap becomes a hold in ms */
+#define TAPPING_TERM 150
